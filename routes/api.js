71cb6240721e66e74ff7b9a26080ddbdeb70a814
Dennis Johnson <dennis@songawee.com>
2014-01-12 10:26:20 -0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,5 @@ exports.hi = function(req, res) {
   fs.writeFile('./imgs/' + 'out' + req.body.index + '.png', req.body.imageData, 'base64', function(err) {
     console.log(err);
   });
-  res.send({cats: 'dog'});
-  console.log('hi');
+  res.send({success: 'true'});
 };
